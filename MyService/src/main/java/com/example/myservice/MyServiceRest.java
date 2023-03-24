d7b214ede87958e9f7eae42bac470c05a3ac3856
Fabinours <44270057+Fabinours@users.noreply.github.com>
2023-03-24 11:45:43 +0100

    Step 1 & 3

--- MyService/src/main/java/com/example/myservice/MyServiceRest.java
+++ MyService/src/main/java/com/example/myservice/MyServiceRest.java
@@ -2,13 +2,27 @@ package com.example.myservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.File;
 
 @RestController
 public class MyServiceRest {
 
     @GetMapping("/")
-    public String sayHello(){
-        return "Hello";
+    public String sayHello() throws java.io.IOException {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("index.html");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
     }
-
 }
